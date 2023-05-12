6a5f422ee73d841bf17cb600716284ca3f47bbaf
scdorvil23 <sydneyd23999@gmail.com>
2023-05-12 13:16:49 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sydney Dorvil23 on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
